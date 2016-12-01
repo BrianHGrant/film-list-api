b4c29604c502665f8f5f8b28ea6d6b935b45e178
BrianHGrant <bhgrant8@gmail.com>
2016-12-01 09:58:20 -0800

    continue with updating class methods

--- filmlist/films/serializers.py
+++ filmlist/films/serializers.py
@@ -1,16 +1,42 @@
 from rest_framework import serializers
 from films.models import Film, Theater, Genre
 
+
+class FilmSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Film
+        fields = ('id', 'title', 'year_prod', 'genre')
+        depth = 1
+
+class GenreFilmSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Film
+        fields = ('id', 'title')
+
 class GenreSerializer(serializers.ModelSerializer):
+    film_set = GenreFilmSerializer(many=True, read_only=True)
     class Meta:
         model = Genre
         fields = ('id', 'title', 'description', 'film_set')
 
-class FilmSerializer(serializers.ModelSerializer):
+class GenreWriteSerializer(serializers.ModelSerializer):
+    film_set = serializers.PrimaryKeyRelatedField(queryset=Film.objects.all(), many=True, allow_null=True)
+    class Meta:
+        model = Genre
+        fields = ('id', 'title', 'description', 'film_set')
+
+class FilmWriteSerializer(serializers.ModelSerializer):
     genre = serializers.PrimaryKeyRelatedField(queryset=Genre.objects.all(), allow_null=True)
+
     class Meta:
         model = Film
-        fields = ('id', 'title', 'year_prod', 'genre', 'theaters')
+        fields = ('id', 'title', 'year_prod', 'genre')
+
+class TheaterFilmSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Film
+        fields = ('id', 'title')
 
 class TheaterSerializer(serializers.ModelSerializer):
     film_set = FilmSerializer(many=True, read_only=True)
@@ -18,10 +44,8 @@ class TheaterSerializer(serializers.ModelSerializer):
         model = Theater
         fields = ('id', 'name', 'city', 'state', 'film_set')
 
-
-class FilmWriteSerializer(serializers.ModelSerializer):
-    genre = serializers.PrimaryKeyRelatedField(queryset=Genre.objects.all(), allow_null=True)
-    theaters = TheaterSerializer(many=True)
+class TheaterWriteSerializer(serializers.ModelSerializer):
+    film_set = serializers.PrimaryKeyRelatedField(queryset=Theater.objects.all(), many=True, allow_null=True)
     class Meta:
-        model = Film
-        fields = ('id', 'title', 'year_prod', 'genre', 'theaters')
+        model = Theater
+        fields = ('id', 'name', 'city', 'state', 'film_set')
--- filmlist/films/views.py
+++ filmlist/films/views.py
@@ -1,29 +1,45 @@
 from films.models import Film, Theater, Genre
-from films.serializers import FilmSerializer, TheaterSerializer, GenreSerializer
+from films.serializers import FilmSerializer, TheaterSerializer, GenreSerializer, FilmWriteSerializer, GenreWriteSerializer
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
-# from films.serializers import FilmSerializer, TheaterSerializer, GenreSerializer, FilmWriteSerializer
 
 class FilmList(generics.ListCreateAPIView):
     queryset = Film.objects.all()
     serializer_class = FilmSerializer
 
+    # def get(self, request, *args, **kwargs):
+    #     year_prod = request.GET.get('year_prod', ' ')
+    #     year_prod__gte = request.GET.get('year_prod__gte', ' ')
+    #     title = request.GET.get('title', ' ')
+    #     films = Film.objects.all()
+    #     if year_prod != ' ':
+    #         films = Film.objects.filter(year_prod=year_prod)
+    #     if year_prod__gte != ' ':
+    #         films = Film.objects.filter(year_prod__gte=year_prod__gte)
+    #     if title != ' ':
+    #         films = films.filter(title__istartswith=title)
+    #     serialized_films = FilmSerializer(films, many=True)
+    #     return Response(serialized_films.data)
+
+    def get_serializer_class(self):
+        if(self.request.method == 'GET'):
+            return FilmSerializer
+        return FilmWriteSerializer
+
     def get(self, request, *args, **kwargs):
-        year_prod = request.GET.get('year_prod', ' ')
-        year_prod__gte = request.GET.get('year_prod__gte', ' ')
-        title = request.GET.get('title', ' ')
-        films = Film.objects.all()
-        if year_prod != ' ':
-            films = Film.objects.filter(year_prod=year_prod)
-        if year_prod__gte != ' ':
-            films = Film.objects.filter(year_prod__gte=year_prod__gte)
-        if title != ' ':
-            films = films.filter(title__istartswith=title)
-        serialized_films = FilmSerializer(films, many=True)
-        return Response(serialized_films.data)
+        k = request.GET.keys() #get the keys from url
+        filter_dict = {}  #create empty dictonary to hold values
+        if(k): # checks if keys exist
+            for key, value in request.GET.items(): # loops through all keys
+                filter_dict[key] = value # adds the values =  filter type of object
+            films = Film.objects.filter(**filter_dict) #returns the films filtered by selection
+            serialized_films = FilmSerializer(films, many=True) # return the serialized film objects
+            return Response(serialized_films.data) #returns to client
+        else:
+            return Response(FilmSerializer(Film.objects.all(),many=True).data) # if no keys, returns unfiltered list of films
 
 class FilmDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Film.objects.all()
@@ -33,6 +49,18 @@ class TheaterList(generics.ListCreateAPIView):
     queryset = Theater.objects.all()
     serializer_class = TheaterSerializer
 
+    def get(self, request, *args, **kwargs):
+        k = request.GET.keys()
+        filter_dict = {}
+        if(k):
+            for key, value in request.GET.items():
+                filter_dict[key] = value
+            theaters = Theater.objects.filter(**filter_dict)
+            serialized_theaters = TheaterSerializer(theaters, many=True)
+            return Response(serialized_theaters.data)
+        else:
+            return Response(TheaterSerializer(Theater.objects.all(), many=True).data)
+
 class TheaterDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Theater.objects.all()
     serializer_class = TheaterSerializer
@@ -40,6 +68,24 @@ class TheaterDetail(generics.RetrieveUpdateDestroyAPIView):
 class GenreList(generics.ListCreateAPIView):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+
+    def get_serializer_class(self):
+        if(self.request.method == 'GET'):
+            return FilmSerializer
+        return GenreWriteSerializer
+
+    def get(self, request, *args, **kwargs):
+        k = request.GET.keys()
+        filter_dict = {}
+        if(k):
+            for key, value in request.GET.items():
+                filter_dict[key] = value
+            genres = Genre.objects.filter(**filter_dict)
+            serialized_genres = GenreSerializer(genres, many=True)
+            return Response(serialized_genres.data)
+        else:
+            return Response(GenreSerializer(Genre.objects.all(), many=True).data)
+
 
 class GenreDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Genre.objects.all()
